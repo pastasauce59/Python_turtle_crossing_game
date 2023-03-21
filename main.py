eb02c787abcab7f271e923d29f37c9cae57509c9
Michael Primavera <pastasauce59@gmail.com>
2023-03-20 23:37:57 -0400

    Created attributes and method for CarManager Class to generate a new car along the y-axis and in a random color that move across the screen from right to left.

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+cars = []
 
 screen.listen()
 screen.onkey(player.move_up, 'Up')
 
 game_is_on = True
+loop = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    if loop % 6 == 0:
+        new_car = CarManager()
+        cars.append(new_car)
+
+    for car in cars:
+        car.move()
+    
+    loop += 1
 screen.exitonclick()
